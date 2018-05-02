785b87051c9f0c0a08591403b816909688bcfdeb
Revortni <rojit.blaze13@gmail.com>
2018-05-02 20:06:05 +0545

    added displaying number of users connected in terminal

--- server.js
+++ server.js
@@ -1,30 +1,48 @@
 var express=require('express');//function handler
 var app = express();
 var http = require('http').Server(app);
-var socket=require('socket.io');
-var io = socket(http);
+var io=require('socket.io')(http);
 app.use(express.static('public'));
+
+//home page routing
 app.get('/',(req,res)=>	res.sendFile(__dirname+'/public/index.html'));
 
-// io.on('connection',msgHandler);
+var userCount=0;
+
 io.on('connection',newconnection);
 
+//functions to call on connection
 function newconnection(socket){
 	var user=socket.id;
+	userCount++;
+
+	console.log("Number of users connected:"+userCount);
+	//when user connects
 	socket.broadcast.emit('userConnect',user);
+
+	//when user sends a message
 	socket.on('chat msg',function(msg){
 		socket.broadcast.emit('chat msg',msg);
 	});
+
+	//when user starts to type into the input panel
 	socket.on("typing",(val)=>{
 		var props={
-			show:val,
+			show:val,// show or hide typing
 			id:user
 		};
 		socket.broadcast.emit("typing",props)
 	});
-	socket.on("disconnect",()=>socket.broadcast.emit('userDisconnect',user));
+
+	//when user disconnects from server
+	socket.on("disconnect",()=>{
+		socket.broadcast.emit('userDisconnect',user);
+		userCount--;
+		console.log("Number of users connected:"+userCount);
+	});
 }
 
 http.listen(3000,function(){
 	console.log('listening on port 3000');
 });
+
