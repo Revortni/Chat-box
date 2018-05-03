088efbb154d5f67fc7de1f27a02dd7b839f76d59
Revortni <rojit.blaze13@gmail.com>
2018-05-03 17:37:35 +0545

    added username feature

--- public/index.js
+++ public/index.js
@@ -1,6 +1,11 @@
 $(function(){
 	var socket=io();
-	var type=0;
+	var username;
+	if (username === undefined) {
+        username = prompt("Enter a username:", "Thanos");
+        socket.emit('userConnect', username); //emit username to server
+    }
+
 	$('#msg').focus();
 	$('form').submit(()=>{
 		var message=$('#msg').val();
@@ -21,27 +26,28 @@ $(function(){
 			 // $('#messages').append($('<li>').text(msg));
 	});
 
-	socket.on('userConnect',(id)=>{
-		var x = document.getElementById("typing");
-		x.style.display='none';
+	socket.on('userConnect',(data)=>{
+		(document.getElementById("typing")).style.display='none';
 		var msg=document.createElement('li');
 		$(msg).addClass('connect');
-		$(msg).html(id+" has joined the chat box.");
+		$(msg).html(data.name+" has joined the chat box");
 		$('#messages').append(msg);
+		var info =msg.cloneNode(true);
+		$(info).html(data.count+" users are currently connected");
+		$('#messages').append(info);
 	});
 
 	socket.on('userDisconnect',(id)=>{
 
 		var msg=document.createElement('li');
 		$(msg).addClass('connect');
-		$(msg).html(id+" has left the chat box.");
+		$(msg).html(id+" has left the chat box");
 		$('#messages').append(msg);
 
 	});
 
 	socket.on('typing',(props)=>{
 		var x = document.getElementById("typing");
-		console.log(props.show);
 		x.style.display=props.show;
 		$('#typing').html(props.id+' is typing');
 	});
@@ -50,7 +56,7 @@ $(function(){
 	$("#msg").keydown(function(){
 		clearTimeout(timer);
 		socket.emit("typing",'block');
-		timer=setTimeout(send,500);
+		timer=setTimeout(send,900);
 	});
 
 	function send()
--- server.js
+++ server.js
@@ -8,35 +8,43 @@ app.use(express.static('public'));
 app.get('/',(req,res)=>	res.sendFile(__dirname+'/public/index.html'));
 
 var userCount=0;
-
+var userlist={};
 io.on('connection',newconnection);
 
 //functions to call on connection
 function newconnection(socket){
-	var user=socket.id;
+	console.log("called");
 	userCount++;
 
 	console.log("Number of users connected:"+userCount);
-	//when user connects
-	socket.broadcast.emit('userConnect',user);
 
+	//when user connects
+	socket.on('userConnect', (username) => {
+        userlist[socket.id] = username;
+        var data = {
+            name: username,
+            count: userCount,
+        };
+        socket.broadcast.emit('userConnect', data);
+    });
+	
 	//when user sends a message
-	socket.on('chat msg',function(msg){
-		socket.broadcast.emit('chat msg',msg);
+	socket.on('chat msg',function(data){
+		socket.broadcast.emit('chat msg',data);
 	});
 
 	//when user starts to type into the input panel
 	socket.on("typing",(val)=>{
-		var props={
+		var data={
 			show:val,// show or hide typing
-			id:user
+			id:userlist[socket.id],
 		};
-		socket.broadcast.emit("typing",props)
+		socket.broadcast.emit("typing",data)
 	});
 
 	//when user disconnects from server
 	socket.on("disconnect",()=>{
-		socket.broadcast.emit('userDisconnect',user);
+		socket.broadcast.emit('userDisconnect',userlist[socket.id]);
 		userCount--;
 		console.log("Number of users connected:"+userCount);
 	});
